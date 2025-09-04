21f3f65de3ef8a14a5047762291af88ab11c6dc5
Aziguy <monsieur_hernandez@hotmail.com>
2025-09-04 16:34:07 +0200

    evo: dataTypes.

---
+++ JavaScriptBasics/dataTypes.js
@@ -0,0 +1,27 @@
+/**
+ * Primitive data types
+ */
+
+/**
+ * Reference data type
+ * Object
+ * Array
+ * Function
+ * Date
+ * Regular Expression
+ * Other object
+ */
+
+let num1 = 30;
+let num2 = 1.333;
+console.log(num1, num2);
+
+let myCity = 'Hanoï';
+let welcomeMessage = `Welcome to ${myCity}`;
+console.log(welcomeMessage);
+
+let trueValue = true;
+let falseValue = false;
+
+let x;
+console.log(x); // undefined
