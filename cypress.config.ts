56ccc3a6ec42d4e5899a49992397f35e5da4674c
Aziguy <monsieur_hernandez@hotmail.com>
2025-09-08 15:04:56 +0200

    feat: config Cypress E2E project

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    /*setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },*/
+    baseUrl: "https://test-qa.macollecte.fr/b/my-donation?_cv=1",
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.{ts,js}",
   },
